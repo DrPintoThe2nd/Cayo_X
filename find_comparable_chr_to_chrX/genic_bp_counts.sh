3e2dec58bafd3d11d618d6737e456af0903f2862
Brendan J. Pinto <18681591+DrPintoThe2nd@users.noreply.github.com>
2021-12-17 11:10:28 -0600

    Update genic_bp_counts.sh

--- find_comparable_chr_to_chrX/genic_bp_counts.sh
+++ find_comparable_chr_to_chrX/genic_bp_counts.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 echo "ensuring conda environment is active."
-source activate Cayo
+conda activate Cayo
 
 echo "downloading gff file."
 wget http://ftp.ensembl.org/pub/release-105/gff3/macaca_mulatta/Macaca_mulatta.Mmul_10.105.gff3.gz;
@@ -9,14 +9,22 @@ echo "unzipping gff using pigz."
 pigz -d Macaca_mulatta.Mmul_10.105.gff3.gz;
 
 echo "converting to bed file using bedops."
-gff2bed < Macaca_mulatta.Mmul_10.105.gff3 | grep 'gene' > Macaca_mulatta.Mmul_10.105.bed;
+gff2bed < Macaca_mulatta.Mmul_10.105.gff3 | grep 'mRNA' > Macaca_mulatta.Mmul_10.105.transcripts.bed;
 bedextract --list-chr Macaca_mulatta.Mmul_10.105.bed > chr.txt;
+
+echo "extracting bed file per chromosome..."
+mkdir per_chr_beds
+cat chr.txt | while read line
+do
+bedextract $line Macaca_mulatta.Mmul_10.105.bed > per_chr_beds/chr$line\_transcripts.bed
+done
 
 echo "extracting bp counts per chromosome..."
 cat chr.txt | while read line
 do
-bedextract $line Macaca_mulatta.Mmul_10.105.bed | awk '/gene/ { len +=$2-$3} END {print len}' >> counts.txt
+bedextract $line Macaca_mulatta.Mmul_10.105.transcripts.bed | awk '/mRNA/ { len +=$2-$3} END {print len}' >> counts.txt
 done
+
 paste chr.txt counts.txt > size.txt
 sed -i 's/\-//g' size.txt
 echo "sorting output."
